a0fb51e24443e2acbf6d3c09c2672f41254b636a
Kurnia Ramadhan Putra <kurniaramadhan@itenas.ac.id>
2022-12-20 10:46:28 +0700

    initiate commit project

---
+++ AppsBangunDatar/src/main/java/com/itenas/appsbangundatar/BangunDatar.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.itenas.appsbangundatar;
+
+/**
+ *
+ * @author Kurnia R. Putra
+ */
+abstract class BangunDatar {
+    private String namaBangunDatar;
+
+    public BangunDatar() {
+    }
+
+    public String getNamaBangunDatar() {
+        return namaBangunDatar;
+    }
+
+    public void setNamaBangunDatar(String namaBangunDatar) {
+        this.namaBangunDatar = namaBangunDatar;
+    }
+    
+    
+    abstract double luas();
+    abstract double keliling();
+}
